71dae375a3a8346e2d97ab050977ac45e6fea107
simonpaolo <simonelopez@hotmail.it>
2022-10-24 11:25:52 +0200

    esercizi

--- src/lezioniSilvia/Esercizi/Esercizio7.java
+++ src/lezioniSilvia/Esercizi/Esercizio7.java
@@ -1,5 +1,50 @@
 package lezioniSilvia.Esercizi;
 
-public class Esercizio7 {
 
+
+public class Esercizio7 {
+	
+	//Programma per matrici e si richiede di implementare i seguenti metodi
+	
+	//il metodo leggi(m) riceve una matrice già allocata nel main e provvede a riempirla attraverso letture da input
+	static void leggi(int[][]m) {}
+	
+	//il metodo scrivi(m) riceve una matrice e ne visualizza il contenuto come meglio credi //fatto
+	static void scrivi(int[][]m) {}
+	
+	//il metodo trasposta(m) ricweve una matrice m, costruisce e ritorna una "nuova" matrice con il contenuto trasposto di m
+	static int[][]trasposta(int[][]m){}
+	
+	//fa la moltiplicazione //fatto è possibile fissarne una senza inserirle tutte due da input
+	static int[][] moltiplica(int[][]m1,int[][]m2){}
+	
+	//fa la somma //fatto è possibile fissarne una senza inserirle tutte due da input
+	static int[][] somma(int[][]m1,int[][]m2){}
+	
+	//ritorna una nuova matrice dove ogni elemento di m1 è moltiplicato per uno scalare
+	static int[][]moltiplicaScalare(int[][]m1,int s){}
+	
+	//una matrice quadrata è simmetrica se coincide con la sua trasposta
+	static boolean simmetrica(int[][]m) {}
+	
+	
+	//il metodo restituisce i punti di sella di una matrice 
+	//i punti di sella sono quegli indici che corrispondo ad un punto che sia maggiore di tutti i valori nella sua riga ma minore di tutti i valori nella sua colonna
+	//esempio in questo caso il punto di sella è [0,2]
+//	123,
+//	345,
+//	678,
+	static int[] puntiDiSella(int[][]m) {}
+	
+	//Quadrato magico:
+	//una matrice quadrata si chiama quadrato magico se la somma degli elementi di una qualsiasi riga, di una qualsiasi colonna, delle due diagonali è sempre la stessa, "costante magica"
+//	1,1,1
+//	1,1,1
+//	1,1,1
+//costante magica =3
+	static boolean isMagicSquare(int[][]m){}
+	
+	public static void main(String []args) {
+		//che implementi tutti i metodi sopra creati
+	}
 }
--- src/lezioniSilvia/Matrici.java
+++ src/lezioniSilvia/Matrici.java
@@ -41,6 +41,63 @@ public class Matrici {
 		//1,2,3
 		//4,5,6
 		//7,8,9
+		
+		//punto della situazione
+		//m[i][j]=elemento in riga i e colonna j
+		//m[i]= tutta la riga
+		for(int i=0;i<m.length;i++) {
+			for(int j=0;j<m[0].length;j++) {
+				m[i][j]=(i==j)?1:0;
+			}
+		}
+		
+		
 	}
+	static int[][] sommmaMatrici(int[][]m1,int[][]m2){
+		if(m1.length!=m2.length || m1[0].length!=m2.length) {
+			System.out.println("dimensioni diverse");
+			System.exit(-1);
+		}
+		int[][]res=new int[m1.length][m1[0].length];
+		for(int i=0;i<m1.length;i++) {
+			for(int j=0;j<m1[0].length;j++) {
+				res[i][j]=m1[i][j]+m2[i][j];
+			}
+		}
+		return res;
+	}
+	
+	//matrice quadrata= dimensione righe e colonne uguale
+	static int[][]moltiplicazione(int[][]m1,int[][]m2){
+		if(m1.length!=m2[0].length) {
+			System.out.println("non è possibile moltiplicare queste due matrici");
+			System.exit(-1);
+		}
+		int [][]matrixRes=new int[m1.length][m1.length];
+		for(int i=0;i<m1.length;i++) {
+			for(int j=0;j<m1[0].length;j++) {
+				int res=0;
+				for(int k=0;k<matrixRes.length;k++) {
+					res=res+m1[i][k]*m2[k][j];
+				}
+				matrixRes[i][j]=res;
+			}
+		}
+		return matrixRes;
+	}
+//{
+//	{1,2,3},
+//	{3,4,5},
+//	{6,7,8}
+//}
+//{
+//	{1,2,3},
+//	{3,4,5},
+//	{6,7,8}
+//}
+//{
+	//{res1,res2,res3}
+	//
+//}
 
 }
